b7c3f1b8c0f1559a4d15ddedf6eb90d67e895d2c
Adarsh-G-P <adarshgp96@gmail.com>
2023-05-31 10:38:34 +0530

    Display alive

--- test_gol.py
+++ test_gol.py
@@ -7,3 +7,7 @@ def test_display_dead():
     expected_output = '***\n***\n***'
     assert gol.display_board(board) == expected_output
 
+def test_display_alive():
+    board = [[1,1,1],[1,1,1],[1,1,1]]
+    expected_output = '###\n###\n###'
+    assert gol.display_board(board) == expected_output
