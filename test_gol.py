7c81da11abd7a9a0b872fe8476b7773d0e3aa215
Adarsh-G-P <adarshgp96@gmail.com>
2023-06-02 07:33:11 +0530

    Next live/dead update

--- test_gol.py
+++ test_gol.py
@@ -1,7 +1,5 @@
 import gol
 
-import gol
-
 def test_display_dead():
     board = [[0, 0, 0],[0,0,0],[0,0,0]]
     expected_output = '***\n***\n***'
@@ -17,6 +15,6 @@ def test_next_live_or_dead():
     expected_board = [[0,0,0],[1,0,1],[0,1,1]]
     assert gol.next_generation(sample_board) == expected_board
 
-# def test_nieghbours_live_dead():
-#     board = [[0,1,0],[0,1,0],[0,1,0]]
-#     assert gameoflife.count_live_dead_neighbors(board,1,1) == 2
+def test_nieghbours_live_dead():
+    board = [[0,1,0],[0,1,0],[0,1,0]]
+    assert gol.count_live_dead_neighbors(board,1,1) == 2
