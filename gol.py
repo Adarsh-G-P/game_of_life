0135a643155e049fd0716c6762de087c8f4b7f3d
Adarsh-G-P <adarshgp96@gmail.com>
2023-06-02 07:26:10 +0530

    Next generation live/ded updates

--- gol.py
+++ gol.py
@@ -9,3 +9,65 @@ def display_board(board):
                 row_str += '*'
         grid.append(row_str)
     return '\n'.join(grid)
+
+
+def next_generation(board):
+    rows = len(board)
+    cols = len(board[0])
+    updated_board = [[0] * cols for _ in range(rows)]
+
+    for row in range(rows):
+        for col in range(cols):
+            live_neighbours = count_live_dead_neighbors(board, row, col)
+
+            if board[row][col] == 1:
+                if live_neighbours < 2 or live_neighbours > 3:
+                    updated_board[row][col] = 0
+                else:
+                    updated_board[row][col] = 1
+            else:
+                if live_neighbours == 3:
+                    updated_board[row][col] = 1
+
+    return updated_board
+
+
+def count_live_dead_neighbors(board, row, col):
+    rows = len(board)
+    cols = len(board[0])
+    live_neighbors = 0
+    for i in range(max(0, row-1), min(rows, row+2)):
+        for j in range(max(0, col-1), min(cols, col+2)):
+            if board[i][j] == 1 and (i != row or j != col):
+                live_neighbors += 1
+    return live_neighbors
+
+
+
+
+
+# def main():
+#     # Initialize the starting grid
+#     grid = [
+#         [0, 1, 0],
+#         [0, 0, 1],
+#         [1, 1, 1],
+#     ]
+
+
+
+    
+#     while True:
+        
+#         grid = next_generation(grid)
+
+        
+#         print(display_grid(grid))
+#         time.sleep(0.8)
+
+# print("Game Over")
+
+
+
+# if __name__ == "__main__":
+#     main()
--- test_gol.py
+++ test_gol.py
@@ -11,3 +11,12 @@ def test_display_alive():
     board = [[1,1,1],[1,1,1],[1,1,1]]
     expected_output = '###\n###\n###'
     assert gol.display_board(board) == expected_output
+
+def test_next_live_or_dead():
+    sample_board = [[0,1,0],[0,0,1],[1,1,1]]
+    expected_board = [[0,0,0],[1,0,1],[0,1,1]]
+    assert gol.next_generation(sample_board) == expected_board
+
+# def test_nieghbours_live_dead():
+#     board = [[0,1,0],[0,1,0],[0,1,0]]
+#     assert gameoflife.count_live_dead_neighbors(board,1,1) == 2
